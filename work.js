7be968c15341a6547b381c346b5f55d53856ddbb
yvesbraeckman <yves.braeckman.06@gmail.com>
2024-01-17 15:20:06 +0100

    added css and js

---
+++ work.js
@@ -0,0 +1,15 @@
+function myFunction(paragraph) {
+    if(paragraph == 1){
+        document.getElementById("p1").classList.toggle("show");
+    }
+    else if(paragraph == 2){
+        document.getElementById("p2").classList.toggle("show");
+    }
+    else if(paragraph == 3){
+        document.getElementById("p3").classList.toggle("show");
+    }
+    else if(paragraph == 4){
+        document.getElementById("p4").classList.toggle("show");
+    }
+}
+
